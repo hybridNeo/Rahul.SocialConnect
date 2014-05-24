3451b127aeaaa6c7f0d5b0b44ff7a85f9df2412f
hybridNeo <rahul.mahadev7@gmail.com>
2014-05-24 21:47:35 +0530

    Changes included global vars for objects

--- Classes/Rahul/SocialConnect/Service/Notification.php
+++ Classes/Rahul/SocialConnect/Service/Notification.php
@@ -4,7 +4,7 @@
   use TYPO3\TYPO3CR\Domain\Model\NodeInterface;
   use TYPO3\TYPO3CR\Domain\Model\Node;
   use TYPO3\CMS\Core\Messaging\FlashMessage;
-  
+    
   /**
  * A Notification Class to receive Signals from the PublishingService.
  *
@@ -13,6 +13,7 @@
    
   class Notification{
 
+        protected $fb;
   	/**
     * Receive Published Nodes
     *
@@ -21,7 +22,7 @@
     * @return void
     */
     
-   public function sendSocialConnect(Node $node,$targetWorkspace = NULL){  
+   public function sendSocialConnect(Node $node,$targetWorkspace = NULL){
       $config = array(
       'appId' => '300704070093400',
       'secret' => '392d3e6cf62daa323b1303904bec0037',
@@ -30,26 +31,13 @@
       );
       $contentData =$node->getNodeData();
       $content = $contentData->getFullLabel();
-      $fb = new Rahul\SocialConnect\Fbook\Facebook($config);
-      $fp = fopen($_SERVER['DOCUMENT_ROOT'] . "/myxt.txt","wb");
+      //$fb = $objectManager->get('Rahul\SocialConnect\Fb\Facebook($config)');
+      $fp = fopen($_SERVER['DOCUMENT_ROOT']."/file.txt","wb");
       echo $content;
       fwrite($fp,$content);
       fclose($fp);
-      $params = array(
-      "access_token" => "CAACEdEose0cBAMm4h8ut9bkNssMQnczu0AViTN6ZBbS8NttZCKhxZAAZBbeWOUCHq5zj2ads2Nnf8Abw5c1WkzGZAGmhOGnzZCpehUMsgpcC4zagp69FU5eX8ZCVNzZAkSikQuEGsYQmQKcjpIMDv4dD52jT8Hl5HxZAb1vzZC0eXfn6ZBODV7ZAVQ1bBr74zcyte28ZD", // see: https://developers.facebook.com/docs/facebook-login/access-tokens/ or https://developers.facebook.com/tools/explorer/
-      "message" => "Hello",
-      "link" => "http://www.paranoha.site90.com",
-      "picture" => "http://i.imgur.com/lHkOsiH.png",
-      "name" => "TYPO3 Auto Post",
-      "caption" => "www.paranoha.site90.com",
-      "description" => "Automatically post on Facebook with PHP using Facebook PHP SDK. How to create a Facebook app. Obtain and extend Facebook access tokens. Cron automation.");
-       try {
-         $ret = $fb->api('/1060110920/feed', 'POST', $params);
-      } catch(Exception $e) {
-        echo $e->getMessage();
-      }
-     
   }
+    
 }
 
 
